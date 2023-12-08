ccee2f6d63670ec087b004c5c6d50f7c62655f02
Дмитрий <135600868+Filimonov207@users.noreply.github.com>
2023-12-08 19:27:37 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
+<?php
+apache_setenv('no-gzip', 1);
+ini_set('zlib.output_compression', 0);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" href="style.css">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	
+	<a href="Cat.jpg" download="https://github.com/Igor21213/DownloadGame/blob/main/Games.zip" class="silka">Скачать игру</a>
 
+</body>
+</html>
